047244910c59f7d73efd3e9a5b84bef5115fa14e
Naveen Kumar K <cbenaveen@gmail.com>
2022-07-03 10:18:37 +0530

    Added fetch.min.bytes configuration

--- src/main/java/org/cbenaveen/hpr/consumer/HPRKafkaConsumerApplication.java
+++ src/main/java/org/cbenaveen/hpr/consumer/HPRKafkaConsumerApplication.java
@@ -1,4 +1,16 @@
 package org.cbenaveen.hpr.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Properties;
+
 public class HPRKafkaConsumerApplication {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        // Configuration that tells the consumer fetch request will wait until min of
+        // 10 bytes available to read from the server
+        properties.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, "10");
+
+    }
 }
